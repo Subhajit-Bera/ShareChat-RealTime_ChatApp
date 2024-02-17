52d9184f8e52fa4f605c267894a8586bfd10815f
Subhajit <subhabera211@gmail.com>
2024-02-17 13:43:40 +0530

    add chakra

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express=require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const app=express();
-const PORT=4000;
+
 
 app.get("/",(req,res)=>{
     res.send("<h1>Hello</h1>");
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server started at port:${PORT}`)
+app.listen(process.env.PORT || 5000,()=>{
+    console.log(`Server started at port:${process.env.PORT}`)
 })
