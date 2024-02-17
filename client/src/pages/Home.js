51fa113ffbaaf0bcc94198886ecf9e5075faac85
Subhajit <subhabera211@gmail.com>
2024-02-17 19:41:56 +0530

    Signup page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+
+import Login from "../components/User/Login";
+import Signup from "../components/User/Signup";
+
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container maxW="xl" centerContent className="home">
+      <Box
+        d="flex"
+        align="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="3xl">
+          Share Chat
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
   )
 }
 
