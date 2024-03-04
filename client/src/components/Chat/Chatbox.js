b046a30999d27a6074c4de7648f52bf6c40c1d57
Subhajit <subhabera211@gmail.com>
2024-03-05 02:27:27 +0530

    ChatBox for chatting

--- client/src/components/Chat/Chatbox.js
+++ client/src/components/Chat/Chatbox.js
@@ -1,10 +1,23 @@
+import { Box } from "@chakra-ui/layout";
 import React from 'react'
-
-const Chatbox = () => {
+import SingleChat from "./SingleChat";
+import { ChatState } from "../../Context/ChatProvider";
+const Chatbox = ({ fetchAgain, setFetchAgain }) => {
+  const { selectedChat } = ChatState();
+  
   return (
-    <div>
-      Chatbox
-    </div>
+    <Box
+      display={{ base: selectedChat ? "flex" : "none", md: "flex" }}
+      alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <SingleChat fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+    </Box>
   )
 }
 
--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,17 +1,18 @@
 import { Box } from "@chakra-ui/layout";
-import React from 'react'
+import {useState} from 'react'
 import { ChatState } from "../Context/ChatProvider";
 import SideBar from '../components/Chat/SideBar';
 import MyChats from '../components/Chat/MyChats';
 import Chatbox from '../components/Chat/Chatbox';
 const Chat = () => {
   const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
   return (
     <div style={{ width: "100%" }}>
       {user && <SideBar />}
       <Box display="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
-        {user && <MyChats/>}
-        {user && <Chatbox/>}
+        {user && <MyChats fetchAgain={fetchAgain}/>}
+        {user && <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>}
       </Box>
     </div>
   )
